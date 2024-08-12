c07dd54e2a9ad0e0aa7f47d74e61fd19ccd9d75d
Kaleidoscopee <romanj1997@gmail.com>
2024-08-11 23:59:54 -0400

    cold button works, hot does not

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 fetch(`https://api.sampleapis.com/coffee/hot`)
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    const randomDrink = document.getElementById('randomDrink');
+    randomDrink.innerHTML = '';
+    data.toGet(drink => {
+        const drinkName = document.createElement('div');
+        drinkName.textContent = drink.title;
+        randomDrink.appendChild(drinkName);
+        });
+    })
+    
   .catch(error => console.log("Error: " + error));
+
+  
