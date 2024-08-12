5fde7750537edc90846ea8244fdbee8c44ecfc76
Kaleidoscopee <romanj1997@gmail.com>
2024-08-12 11:21:13 -0400

    added DOM event listener so the code actually prints

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const pickButton = document.getElementById('pick_button');
+document.addEventListener('DOMContentLoaded', () => {
+  const pickButton = document.getElementById('pick_button');
 
-pickButton.addEventListener('click', () => {
-  fetch(`https://api.sampleapis.com/coffee/hot`)
-  .then(res => res.json())
-  .then(data => {
-    const randomDrink = document.getElementById('randomDrink');
-    randomDrink.innerHTML = '';
-    data.toGet(drink => {
-        const drinkName = document.createElement('div');
-        drinkName.textContent = drink.title;
-        randomDrink.appendChild(drinkName);
-        });
-    })
+  pickButton.addEventListener('click', () => {
+    fetch(`https://api.sampleapis.com/coffee/hot`)
+    .then(res => res.json())
+    .then(data => {
+     const randomIndex = Math.floor(Math.random() * data.length);
+     const randomDrinkData = data[randomIndex];
     
-  .catch(error => console.log("Error: " + error));
+      const randomDrink = document.getElementById('randomDrink');
+      randomDrink.innerHTML = '';
+    
+          const drinkName = document.createElement('div');
+          drinkName.textContent = randomDrinkData.title;
+          randomDrink.appendChild(drinkName);
+        
+      })
+    
+    .catch(error => console.log("Error: " + error));
 
 
+  });
 })
+
 
 
  /* fetch(`https://api.sampleapis.com/coffee/iced`)
