9eb2de5b8b265014dd739688d1c9ac30f46bac73
Kaleidoscopee <romanj1997@gmail.com>
2024-08-12 10:22:36 -0400

    put my fetch into an event listener so my pick for me button works

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Just simply choose between Hot or Cold, and then click the "Click Here" to be su
 # Journey Journal
 
 Note that this journey goes over multiple Vite attempts and it was decided to scale back and do barebones vanilla JS, CSS, and HTML to be able to turn in the project on time. 
+
+Okay so Hot and Cold works so far, just have to find a way to not only get the api to print to console, but to appear in html consistently. Will probably need to randomize with something like math.random. Will take a quick nap on it since once I can get it working on one it should work on both since the only difference is the end of the URL from hot to iced. 
--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-fetch(`https://api.sampleapis.com/coffee/hot`)
+const pickButton = document.getElementById('pick_button');
+
+pickButton.addEventListener('click', () => {
+  fetch(`https://api.sampleapis.com/coffee/hot`)
   .then(res => res.json())
   .then(data => {
     const randomDrink = document.getElementById('randomDrink');
@@ -11,5 +14,23 @@ fetch(`https://api.sampleapis.com/coffee/hot`)
     })
     
   .catch(error => console.log("Error: " + error));
+
+
+})
+
+
+ /* fetch(`https://api.sampleapis.com/coffee/iced`)
+  .then(res => res.json())
+  .then(data => {
+    const randomDrink = document.getElementById('randomDrink');
+    randomDrink.innerHTML = '';
+    data.toGet(drink => {
+        const drinkName = document.createElement('div');
+        drinkName.textContent = drink.title;
+        randomDrink.appendChild(drinkName);
+        });
+    })
+    
+  .catch(error => console.log("Error: " + error));*/
 
   
